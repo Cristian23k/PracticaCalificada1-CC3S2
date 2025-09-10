5be52cff18f0f2daf0c6d0dfaf03087cecd554ed
sergio <sergio@example.com>
2025-09-09 20:00:56 -0500

    Se agrego la creacion del archivo out/reporte.txt

--- src/http_check.sh
+++ src/http_check.sh
@@ -4,4 +4,13 @@ DIRECTORIO_RAIZ="$(dirname "$(dirname "$(realpath "$0")")")"
 DIRECTORIO_SALIDA="${DIRECTORIO_RAIZ}/out/http"
 mkdir -p "${DIRECTORIO_SALIDA}"
 
+#Verificar codigo de estado
+codigo_de_estado="$(curl -s -o /dev/null -w "%{http_code}" "${TARGETS}")"
+if [ "${CODIGO_DE_ESTADO_ESPERADO}" == "${codigo_de_estado}" ]; then
+    echo -e "Codigo de estado correcto: ${codigo_de_estado}\n" > "${DIRECTORIO_SALIDA}/reporte_http.txt"
+else 
+    echo -e "Codigo de estado incorrecto:\n\tEsperado:${CODIGO_DE_ESTADO_ESPERADO}\tRecibido:${codigo_de_estado}" > "${DIRECTORIO_SALIDA}/reporte_http.txt"
+fi
+#Incluye el tiempo de respuesta en el reporte
+curl -s -o /dev/null -w "Tiempo de respuesta: %{time_total}s\n" "${TARGETS}">> "${DIRECTORIO_SALIDA}/reporte_http.txt" 
 curl -sI "${TARGETS}" > "${DIRECTORIO_SALIDA}/cabeceras.txt" #Crea archivo con las cabeceras
