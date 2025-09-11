6ea3dc65920efada72cc44dd8d7915aca3a00a1a
sergio <sergio@example.com>
2025-09-11 16:46:12 -0500

    Se agrego menu a monitor.sh

--- src/monitor.sh
+++ src/monitor.sh
@@ -2,43 +2,74 @@
 
 DIRECTORIO_RAIZ="$(dirname "$(dirname "$(realpath "$0")")")"
 
-echo "Iniciando monitor de seguridad..."
-echo "Target: ${TARGETS} Puerto: ${PORT} Servidor DNS: ${DNS_SERVER}"
-#HTTP
-echo "Ejecutando http_check.sh ..."
-if bash ${DIRECTORIO_RAIZ}/src/http_check.sh; then
-    echo "Se ejecutó el script correctamente"
-else
-    echo "Error al ejecutar el script"
-    exit 1
-fi
-echo "REPORTE:"
-cat "${DIRECTORIO_RAIZ}/out/http/reporte_http.txt"
-sleep 2
-echo "Cabeceras:"
-cat "${DIRECTORIO_RAIZ}/out/http/cabeceras.txt"
-sleep 2
-echo "Cuerpo:"
-cat "${DIRECTORIO_RAIZ}/out/http/cuerpo_http.txt"
-#DNS
-echo "Ejecutando dns_check.sh ..."
-if bash ${DIRECTORIO_RAIZ}/src/dns_check.sh; then
-    echo "Se ejecutó el script correctamente"
-else
-    echo "Error al ejecutar el script"
-    exit 1
-fi
-echo "REPORTE:"
-sleep 2
-cat "${DIRECTORIO_RAIZ}/out/dns/salida_dig.txt"
-#TLS
-echo "Ejecutando tls_check.sh ..."
-if bash ${DIRECTORIO_RAIZ}/src/tls_check.sh; then
-    echo "Se ejecutó el script correctamente"
-else
-    echo "Error al ejecutar el script"
-    exit 1
-fi
-echo "REPORTE:"
-sleep 2
-cat "${DIRECTORIO_RAIZ}/out/tls/info_tls.txt"
+menu() {
+    echo "Target: ${TARGETS} Puerto: ${PORT} Servidor DNS: ${DNS_SERVER}"
+    echo "Ingrese un digito"
+    echo -e "1)http_check\t2)dns_check\t3)tls_check\t4)salir"
+    read opcion
+    case ${opcion} in
+        1)
+            http_check
+            ;;
+        2)
+            dns_check
+            ;;
+        3)
+            tls_check
+            ;;
+        4)
+            echo "Saliendo..."
+            exit 0
+            ;;
+        *)
+            echo "Opcion invalida"
+            ;;
+    esac
+}
+tls_check() {
+    echo "Ejecutando tls_check.sh ..."
+    if bash ${DIRECTORIO_RAIZ}/src/tls_check.sh; then
+        echo "Se ejecutó el script correctamente"
+    else
+        echo "Error al ejecutar el script"
+        exit 1
+    fi
+    echo "REPORTE:"
+    sleep 2
+    cat "${DIRECTORIO_RAIZ}/out/tls/info_tls.txt"
+}
+http_check() {
+    echo "Ejecutando http_check.sh ..."
+    if bash ${DIRECTORIO_RAIZ}/src/http_check.sh; then
+        echo "Se ejecutó el script correctamente"
+    else
+        echo "Error al ejecutar el script"
+        exit 1
+    fi
+    echo "REPORTE:"
+    cat "${DIRECTORIO_RAIZ}/out/http/reporte_http.txt"
+    sleep 2
+    echo "Cabeceras:"
+    cat "${DIRECTORIO_RAIZ}/out/http/cabeceras.txt"
+    sleep 2
+    echo "Cuerpo:"
+    cat "${DIRECTORIO_RAIZ}/out/http/cuerpo_http.txt"
+}
+dns_check() {
+    echo "Ejecutando dns_check.sh ..."
+    if bash ${DIRECTORIO_RAIZ}/src/dns_check.sh; then
+        echo "Se ejecutó el script correctamente"
+    else
+        echo "Error al ejecutar el script"
+        exit 1
+    fi
+    echo "REPORTE:"
+    sleep 2
+    cat "${DIRECTORIO_RAIZ}/out/dns/salida_dig.txt"
+}
+echo "MONITOR DE SEGURIDAD EN REDES"
+while true
+do 
+    menu
+done
+
